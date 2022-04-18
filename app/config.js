74e830d482e993bd44fafa8dead33c0c6e4cf7ed
ptanand <ptanandny@gmail.com>
2022-04-18 16:37:21 -0400

    Commit update

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://obscure-crag-36544.herokuapp.com/',
+  production: 'https://obscure-crag-36544.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
